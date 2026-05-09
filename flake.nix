7e6f968b2aae7297935248f5b74c9219fa6f833e
g0yp <goupsoup2@gmail.com>
2026-05-09 17:15:19 -0500

    Rebuild nix-containers: 2026-05-09-17:15

--- flake.nix
+++ flake.nix
@@ -5,10 +5,6 @@
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
         nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-        opencode = {
-            url = "github:anomalyco/opencode/v1.1.36";
-            inputs.nixpkgs.follows = "nixpkgs-unstable";
-        };
     };
 
     outputs = { self, nixpkgs, nixpkgs-unstable, opencode, ... }@inputs: {
@@ -26,13 +22,6 @@
                     ./configuration.nix
                     ./hosts/docker-nixos/docker-nixos.nix
                     ./hosts/vm.nix
-                    ({ pkgs, ... }: {
-                        environment.systemPackages = [
-                            opencode.packages.x86_64-linux.default
-                        ];
-                    })
-                        
-                        
                 ];
 
             };
